c95fb19534d16cafc3d856b12aee74fb0fa5e219
Hanyu Zhang <133269327+hzhan18@users.noreply.github.com>
2024-11-08 09:41:44 -0800

    updated app.py

--- app.py
+++ app.py
@@ -189,4 +189,5 @@ def logout():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()  # 创建数据库表
-    app.run(debug=True) 
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
